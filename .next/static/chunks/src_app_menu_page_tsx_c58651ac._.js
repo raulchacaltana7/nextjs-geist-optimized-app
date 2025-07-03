4947ee80755fc619f888d778a6615d28df86aafa
raulchacaltana7 <raul.chacaltana7@gmail.com>
2025-07-03 16:33:01 -0500

    Add .next/static/chunks/src_app_menu_page_tsx_c58651ac._.js 1751578381357

---
+++ .next/static/chunks/src_app_menu_page_tsx_c58651ac._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_next_46d16782._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_framer-motion_dist_es_24e93cb0._.js",
+  "static/chunks/node_modules_motion-dom_dist_es_fa3ea29e._.js",
+  "static/chunks/node_modules_04f15718._.js",
+  "static/chunks/src_ad2f42dd._.js"
+],
+    source: "dynamic"
+});
